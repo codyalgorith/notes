5a68b77b85cc172a969368596508a419b4befa77
codyalgorith <31162097+codyalgorith@users.noreply.github.com>
2017-09-04 12:49:11 +0530

    Update nlognlcs.cpp

--- nlognlcs.cpp
+++ nlognlcs.cpp
@@ -1,4 +1,5 @@
 //nlogn lcs
+//important assumption -> array element must be distincts .. i.e.   arr 1 must have all entries distinct and same for arr 2
 //condition elements of both sequences should be distinct 
 /*logic behind this approach is very simple
 take the smaller array and find an array of locations of the elements of this smaller array into larger array
@@ -10,11 +11,14 @@ which is the answer also
 
 using namespace std;
 
-int mapper[50000];
-int a[50000];
-int b[50000];
-int r[50000];
+int mapper[50000];  //we ccan map too ... map will be more safer if the array values lie upto 10^9 or higher
+
+int a[50000];    //array 1
+
+int b[50000];   // array 2
+int r[50000];  //stores the index of elements 
 int k;
+///just lower_bound search
 int ceilbinary(int t[50000],int beg,int end,int item)
 {
 	if(t[end]<item) return -1;
@@ -33,20 +37,22 @@ int ceilbinary(int t[50000],int beg,int end,int item)
 	}
 	return end;
 }
+
+//nlogn lis 
 int lis()
 {
 	int t[50000];
 	int sizeoflis=1;
-	
+	//t does not stores the lis
 	t[0]=r[0];
 	for(int i=1;i<k;++i)
 	{
-		if(r[i]>t[sizeoflis-1])
+		if(r[i]>t[sizeoflis-1]) // if last element in t is smaller than arr[i], append it
 		{
 			t[sizeoflis]=r[i];
 			sizeoflis++;
 		}
-		else{
+		else{  //else search the pos of eleement >= arr[i ] in t
 			int pos=ceilbinary(t,0,sizeoflis-1,r[i]);
 			t[pos]=r[i];
 		}
@@ -61,13 +67,14 @@ int main()
 	scanf("%d %d",&n,&m);
 	memset(mapper,-1,sizeof(mapper));
 	int answer;
-if(n>m)
+	//we will map the arrelement -> index of smaller array 
+if(n>m)   // so this check
 {
 		
 	for(int i=0;i<n;++i)
 	{
 		scanf("%d",&a[i]);
-		mapper[a[i]]=i;
+		mapper[a[i]]=i;  // mapped elements of arr a into indexes
 	}
 	 k=0; //size of array r
 	for(int j=0;j<m;++j)
@@ -76,7 +83,7 @@ if(n>m)
 		
 		if(mapper[b[j]]!=-1)
 		{
-			r[k]=mapper[b[j]];
+			r[k]=mapper[b[j]];  //made an array using indexes
 			k++;
 		}
 	}
